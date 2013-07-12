5f503bd7a4148213e9cb2fdd74630cb0cb7ace1f
whaleyuding <whale.yu.ding@gmail.com>
2013-07-12 03:30:06 +0000

    Add content in index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buf = require('buffer'); 
+
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html');
+
+var string = buf.toString('utf-8')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
