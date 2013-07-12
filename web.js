7300c300b9dfcc430e60fcc4d556466566358081
whaleyuding <whale.yu.ding@gmail.com>
2013-07-12 03:38:00 +0000

    Modified with web.js

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 
 var fs = require('fs');
 
-var buf = require('buffer'); 
+var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 fs.readFileSync('index.html');
 
-var string = buf.toString('utf-8')
+var string = buf.toString('utf-8');
 
 app.get('/', function(request, response) {
   response.send(string);
